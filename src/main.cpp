ef428f93256c90d42dc3cb5baac22834b08d553c
CVJT <CVJT@users.noreply.github.com>
2017-03-24 17:00:15 +0000

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Chris Tilley on 13/03/2017.
+//
+
+
+#include <fstream>
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+  string fileName;
+
+  cout << "Input the file location:";
+  cin >> fileName;
+
+ // ?????::readFile(fileName);
+
+  //readFile("/Users/Tilps007/Development/HillSheepStudios/Blofeld/TestFiles/Test1.txt");
+
+  return 0;
+}
+
---
+++ tests/UnitTests/Test1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Chris Tilley on 01/09/2016.
+//
+
+#include "gtest/gtest.h"
+
+TEST(SuiteName, TestName) {
+  
+}
